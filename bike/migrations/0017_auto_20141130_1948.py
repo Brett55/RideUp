e117fad2f9f13ccfe7b348840595f3340ba9da60
brett55 <bjcull2@gmail.com>
2014-11-30 16:56:18 -0500

    got form with fake

---
+++ bike/migrations/0017_auto_20141130_1948.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bike', '0016_auto_20141130_1946'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='ridelocation',
+            name='rideType',
+            field=models.CharField(max_length=50, verbose_name=b'Type of Ride/Event', choices=[(b'RACE', b'RACE'), (b'GROUP_RIDE', b'Group Ride'), (b'TRAIL_WORK DAY', b'Trail Work Day'), (b'BIKE_SWAP', b'Bike Swap'), (b'SPECIAL_EVENT', b'Special Event'), (b'CONFERENCE', b'Conference')]),
+            preserve_default=True,
+        ),
+    ]
