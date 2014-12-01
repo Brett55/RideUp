d453d87c6d554446e971f8376480eada44d345ee
brett55 <bjcull2@gmail.com>
2014-12-01 15:33:32 -0500

    form submits correctly for group_ride_trail

--- bike/urls.py
+++ bike/urls.py
@@ -5,9 +5,14 @@ from models import RideLocation
 
 urlpatterns = patterns('',
                        url(r'^(?P<pk>\d+)/$', 'bike.views.moreData', name='data'),
-                       url(r'^form/(?P<type>\w+)/$', 'bike.views.form_updater', name='data'),
+                       url(r'^form/(?P<type>\w+)/$', 'bike.views.form_updater'),
                        url(r'^data.geojson/$', GeoJSONLayerView.as_view(model=RideLocation, properties=('name'))),
-                       url(r'^add_point/$', 'bike.views.add_point'),
+
+
+                       url(r'^index/$', 'bike.views.index'),
+                       url(r'^group_ride_trail/$', 'bike.views.group_ride_trail'),
+
+
                        url(r'^add_point/error/$', 'bike.views.form_error'),
                        url(r'^add_point/success/$', 'bike.views.form_success'),
 )
--- bike/views.py
+++ bike/views.py
@@ -9,32 +9,43 @@ from django.contrib.auth.models import User
 from django.template import Context, Template
 
 
-def add_point(request):
+def index(request):
+    args = {}
+    args.update(csrf(request))
+    args['kickoff'] = forms.KickOff()
+    return render_to_response('bike/index.html', args)
+
+
+def group_ride_trail(request):
     if request.method == 'POST':
+        form_kickoff = forms.KickOff(request.POST)
         form = forms.AddRideSpotTrail(request.POST)
-        if form.is_valid():
+        if form.is_valid() and form_kickoff.is_valid():
             new_point = models.RideLocation()
 
             cd = form.cleaned_data
-            coordinates = cd['coordinates'].split(',')
+            cd_form_kickoff = form_kickoff.cleaned_data
+
+            coordinates = cd_form_kickoff['coordinates'].split(',')
             new_point.geom = Point(float(coordinates[0]), float(coordinates[1]))
-            new_point.name = cd['name']
+            new_point.name = cd_form_kickoff['name']
+            new_point.rideType = cd_form_kickoff['rideType']
             new_point.save()
 
             new_ride = models.GroupRideDirt()
-            new_ride.location = new_point
+            new_ride.location = new_point #1
             new_ride.save()
 
             username = cd['username']
             password = cd['password']
             newGuy = User.objects.create_user(username, "adwa@gmail.com", password)
-            new_ride.riders.add(newGuy)
+            new_ride.riders.add(newGuy) #2
 
-            new_ride.rideTypeMTB = cd['rideTypeMTB']
-            new_ride.roadOrDirt = cd['roadOrDirt']
-            new_ride.rideLevel = cd['rideLevel']
+            new_ride.rideTypeMTB = cd['rideTypeMTB'] #3
+            new_ride.rideLevel = cd['rideLevel'] #4
 
-            new_ride.ridetime = str(cd['ridetime'])
+            new_ride.ridetime = str(cd['ridetime']) #5
+            new_ride.postRideBeer = cd['postRideBeer'] #6
 
             new_ride.save()
 
@@ -43,12 +54,6 @@ def add_point(request):
         else:
             return HttpResponseRedirect('/bike/add_point/error')
 
-    args = {}
-    args.update(csrf(request))
-    args['kickoff'] = forms.KickOff()
-
-    return render_to_response('bike/add_point.html', args)
-
 
 def form_error(request):
     return render_to_response('bike/form_error.html')
@@ -56,24 +61,6 @@ def form_error(request):
 
 def form_success(request):
     return render_to_response('bike/form_success.html')
-
-
-# def moreDataOld(request):
-# ridelocations = models.RideLocation.objects.all()
-#     rideInfos = models.Ride.objects.all()
-#     to_Json = {}
-#     x = 0
-#     for info in rideInfos:
-#         infoDict = {}
-#         infoDict['location'] = str(info.location)
-#         infoDict['rideTypeMTB'] = info.rideTypeMTB
-#         infoDict['roadOrDirt'] = info.roadOrDirt
-#         infoDict['rideLevel'] = info.rideLevel
-#         infoDict['riders'] = str(info.riders.all()[0])
-#         to_Json['obj' + str(x)] = infoDict
-#         x += 1
-#     response_data = simplejson.dumps(to_Json)
-#     return HttpResponse(response_data, content_type="application/json")
 
 
 def moreData(request, pk):
