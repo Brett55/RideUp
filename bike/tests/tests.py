7ed440ab84eea6ae66eb74d9e31e641840cc4f31
brett55 <bjcull2@gmail.com>
2015-03-25 19:45:36 -0400

    more testing w/ selenium

--- bike/tests/tests.py
+++ bike/tests/tests.py
@@ -1,24 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
 from bike.models import RideLocation
 from django.contrib.gis.geos import Point
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import django
+
 django.setup()
+
+
+class AdminTest(LiveServerTestCase):
+    # load fixtures
+    fixtures = ['admin.json']
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_admin_site(self):
+        # user opens web browser, navigates to admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        print (username_field.send_keys('admin'))
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
 
 class GroupRideDirtTestCase(TestCase):
     def setUp(self):
         RideLocation.objects.create(
-            name = "test",
-            event_Frequency = "Weekly",
-            editable = "yes",
-            rideType = "SPECIAL_EVENT",
-            roadOrDirt = "TRAIL",
-            geom = Point(112.32132, 34.24323)
+            name="test",
+            event_Frequency="Weekly",
+            editable="yes",
+            rideType="SPECIAL_EVENT",
+            roadOrDirt="TRAIL",
+            geom=Point(112.32132, 34.24323)
         )
 
     def test_making_ride_point(self):
         """test making of a single ride event"""
         group_ride_one = RideLocation.objects.get(name="test")
         self.assertEqual(group_ride_one.name, 'test')
+
+
 
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.contrib.gis import admin
 
 urlpatterns = [
-    url(r'', include('bike.urls', namespace="bike")),
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('bike.urls', namespace="bike"))
 ]
