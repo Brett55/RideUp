fe47d87d5fe6e1fa3d5bfc020267c6b3c2d64383
brett55 <bjcull2@gmail.com>
2015-03-31 17:23:26 -0400

    Setup tests more to capture HTML elements

--- bike/tests/tests.py
+++ bike/tests/tests.py
@@ -20,19 +20,9 @@ class AdminTest(LiveServerTestCase):
 
     def test_admin_site(self):
         # user opens web browser, navigates to admin page
-        self.browser.get(self.live_server_url + '/admin/')
+        self.browser.get(self.live_server_url)
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Django administration', body.text)
-        # users types in username and passwords and presses enter
-        username_field = self.browser.find_element_by_name('username')
-        print (username_field.send_keys('admin'))
-        username_field.send_keys('admin')
-        password_field = self.browser.find_element_by_name('password')
-        password_field.send_keys('admin')
-        password_field.send_keys(Keys.RETURN)
-        # login credentials are correct, and the user is redirected to the main admin page
-        body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Site administration', body.text)
+        self.assertIn('Ride[up]:', body.text)
 
 
 class GroupRideDirtTestCase(TestCase):
