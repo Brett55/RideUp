8b0cb2ebf0604190f923aa79e95e286468249dfe
brett55 <bjcull2@gmail.com>
2015-04-16 17:25:26 -0400

    refactored in classes for cleaner

--- bike/tests/tests.py
+++ bike/tests/tests.py
@@ -71,6 +71,25 @@ class MainTest(TestCase):
         # close popup
         self.browser.find_element_by_css_selector('.btn-primary').click()
 
+
+class CheckRideDetailsandJoin(TestCase):
+    '''test the ability to view ride details, and join the ride'''
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_exists_by_xpath(self, xpath):
+        try:
+            self.browser.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def test_view_riders(self):
+
         # ride details
         result = self.browser.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div[1]/div[2]/div[3]/div[4]/i")
         result.click()
@@ -97,6 +116,8 @@ class MainTest(TestCase):
 
         self.browser.implicitly_wait(1)
 
+    def test_join_ride(self):
+
         # send form data
         self.browser.find_element_by_css_selector('#joinRideButton').click()
         self.browser.find_element_by_css_selector('#id_first_name').send_keys("NewMemberBobbyHill")
@@ -111,7 +132,6 @@ class MainTest(TestCase):
         self.browser.find_element_by_xpath('/html/body/div[2]/div[1]').click()
 
         # check member form submission now
-
         result = self.browser.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div[1]/div[2]/div[3]/div[4]/i")
         result.click()
         self.browser.find_element_by_css_selector('#joinRideButton').click()
@@ -120,13 +140,14 @@ class MainTest(TestCase):
 
         self.browser.find_element_by_css_selector('#id_first_name').send_keys("NewMemberDaleGribble")
         self.browser.implicitly_wait(1)
-        # generate fake username
+
+        # generate fake username and send
         self.browser.find_element_by_css_selector('#id_username').send_keys("myCoolUserName" +
                                                                             str(randint(0, 9) * randint(0, 9)
                                                                                 * randint(0, 9)))
-
         self.browser.implicitly_wait(1)
 
+        # send password and submit form
         self.browser.find_element_by_css_selector('#id_password').send_keys("blah1234")
         self.browser.implicitly_wait(1)
         self.browser.find_element_by_css_selector('#submitJoinRideForm').click()
@@ -147,20 +168,3 @@ class GroupRideDirtTestCase(TestCase):
         """test making of a single ride event"""
         group_ride_one = RideLocation.objects.get(name="test")
         self.assertEqual(group_ride_one.name, 'test')
-
-
-# class CheckAddPointAndView(TestCase):
-#
-# """test to make sure a person can view ride details by clicking a feature"""
-#
-# def setUp(self):
-# self.browser = webdriver.Firefox()
-#
-#     def test_view_point(self):
-#         result = self.browser.find_element_by_css_selector(".awesome-marker-icon-blue")
-#         result.click()
-
-
-
-
-
