33790201cfad2a25faa89f383a94d7eb2e7bf573
brett55 <bjcull2@gmail.com>
2015-04-17 16:57:58 -0400

    minified JS for prod, next need to do CSS

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            vendor: {
+                src: ['bike/static/bike/js/jquery.js', 'bike/static/bike/js/bootstrap.min.js', 'bike/static/bike/js/leaflet.js',
+                    'bike/static/bike/js/**/*.js'],
+                dest: 'bike/static/bike/prod/js/vendor.js'
+            },
+            app: {
+                src: ['bike/static/bike/scripts/map.js', 'bike/static/bike/scripts/joinRide.js', 'bike/static/bike/scripts/getRiderInfo.js',
+                    'bike/static/bike/scripts/createRide.js', 'bike/static/bike/scripts/editRide.js', 'bike/static/bike/scripts/navBar.js'],
+                dest: 'bike/static/bike/prod/scripts/app.js'
+            }
+        },
+        uglify: {
+            options: {
+                separator: ';'
+            },
+            app: {
+                files: {
+                    'bike/static/bike/prod/scripts/app.min.js': ['bike/static/bike/prod/scripts/app.js']
+                }
+            },
+            vendor: {
+                files: {
+                    'bike/static/bike/prod/js/vendor.min.js': ['bike/static/bike/prod/js/vendor.js']
+                }
+            }
+        }
+    });
+
+    // Load plugins here.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register tasks here.
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
