7340795aa6ccfb9863b00474a6b1e3769c4adc72
brett55 <bjcull2@gmail.com>
2015-11-18 07:25:32 -0700

    added default val

--- RideUp/settings/base.py
+++ RideUp/settings/base.py
@@ -23,7 +23,7 @@ print BASE_DIR
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "wdadwadwadwadwadwa")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
